425ac081c56cfc91f5ab3a2ba3ef44ea04227100
Jolie Gielchinsky <jolieschae@gmail.com>
2023-04-05 13:17:58 -0400

    complete duck manager

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-// write your code here!
+let duckNav = document.getElementById("duck-nav")
+let duckImageDisplay = document.getElementById("duck-display-image")
+let duckNameDisplay = document.getElementById("duck-display-name")
+let likeButton = document.getElementById("duck-display-likes")
+let form = document.getElementById("new-duck-form")
+let newDuckName
+let newDuckImage
+
+fetch("http://localhost:3000/ducks")
+.then(response => response.json())
+.then(data => renderDucks(data));
+
+function renderDucks(ducks){
+    ducks.forEach((duck) => {
+        let duckImage = document.createElement("img");
+        duckImage.src = duck.img_url;
+        duckNav.append(duckImage);
+
+        duckImage.addEventListener("click", () => {
+            duckNameDisplay.textContent = duck.name;
+            duckImageDisplay.src = duck.img_url;
+            likeButton.textContent = duck.likes;
+         });
+    });
+};
+
+likeButton.addEventListener("click", () => {
+    likeButton.textContent++;
+ });
+
+ form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let newValue = parseInt(e.target.likes.value) + parseInt(likeButton.textContent)
+    likeButton.textContent = newValue
+ });
